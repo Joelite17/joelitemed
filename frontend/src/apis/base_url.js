0f4b5133fdef6379c7ef1dd31df4ddc248919e99
Josiah Chinedu <chinedujosiahjkl@gmail.com>
2026-03-07 00:19:50 +0100

    vite corrected

--- frontend/src/apis/base_url.js
+++ frontend/src/apis/base_url.js
@@ -1,3 +1,4 @@
 // Use environment variable for flexibility
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+console.log(BASE_URL)
 export const VITE_PAYSTACK_PUBLIC_KEY = import.meta.env.VITE_PAYSTACK_PUBLIC_KEY || "pk_test_19a21304b3734cb6c4ca98d5b32f0b624c84cce9";
