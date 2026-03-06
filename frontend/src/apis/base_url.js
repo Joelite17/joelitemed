2948eb393146f1579b563575d4e93263f7b30cb1
Josiah Chinedu <chinedujosiahjkl@gmail.com>
2026-03-06 23:51:15 +0100

    base url changed

--- frontend/src/apis/base_url.js
+++ frontend/src/apis/base_url.js
@@ -1,5 +1,6 @@
 
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://joelitemed-frontend-production.up.railway.app/";
+// export const BASE_URL = "http://localhost:8000";
 export const VITE_PAYSTACK_PUBLIC_KEY="pk_test_19a21304b3734cb6c4ca98d5b32f0b624c84cce9"
 // export const VITE_PAYSTACK_PUBLIC_KEY="pk_live_dfec5417e97c4ee3bbf680c06ff06d2ff74c8dcb"
 
