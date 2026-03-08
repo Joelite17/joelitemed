5e214af6dc16f1746b4ca03b63188c2c505560ab
Josiah Chinedu <chinedujosiahjkl@gmail.com>
2026-03-08 09:56:27 +0100

    email problems resolved

--- gitcredentialproblem.py
+++
@@ -1,4 +0,0 @@
-# git filter-repo --path backend/z_email_data/credentials.json --invert-paths --force
-# git remote add origin https://github.com/Joelite17/joelitemed.git
-# git remote -v
-# git push origin main --force
